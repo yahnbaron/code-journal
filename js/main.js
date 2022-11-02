96f0bbd0d0b4ca40e2e315fcacf10a8275ccfc0b
Baron Yahn <yahnbaron@me.com>
2022-11-02 16:14:56 +0000

    finished the view entries functionality.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,59 @@ function subForm(event) {
   theForm.reset();
   theImgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
+
+function renderTodo(entry) {
+  /**
+   * <li class="column-full">
+            <div class="row">
+              <div class="column-half">
+                <img src="" title="ada lovelace">
+              </div>
+              <div class="column-half">
+                <h2>Ada Lovelace</h2>
+                <p>ada ada ada</p>
+              </div>
+            </div>
+          </li>
+  */
+
+  var theListItem = document.createElement('li');
+  theListItem.setAttribute('class', 'column-full');
+
+  var theDivRow = document.createElement('div');
+  theDivRow.setAttribute('class', 'row');
+
+  var theDivHalfCol = document.createElement('div');
+  theDivHalfCol.setAttribute('class', 'column-half');
+
+  var thePic = document.createElement('img');
+  thePic.setAttribute('src', entry.photoURL);
+
+  theListItem.append(theDivRow);
+  theDivRow.append(theDivHalfCol);
+  theDivHalfCol.append(thePic);
+
+  var theOtherDivHalf = document.createElement('div');
+  theOtherDivHalf.setAttribute('class', 'column-half');
+
+  var theTitle = document.createElement('h3');
+  theTitle.textContent = entry.title;
+
+  var theNotes = document.createElement('p');
+  theNotes.textContent = entry.notes;
+
+  theDivRow.append(theOtherDivHalf);
+  theOtherDivHalf.append(theTitle, theNotes);
+
+  return theListItem;
+}
+
+var theList = document.querySelector('.list-parent');
+
+document.addEventListener('DOMContentLoaded', addThem);
+
+function addThem(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    theList.append(renderTodo(data.entries[i]));
+  }
+}
